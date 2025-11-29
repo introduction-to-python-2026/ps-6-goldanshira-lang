24c2f100826bfc9b175cadd2d8e8a2b1821d5b7f
goldanshira-lang <goldanshira@gmail.com>
2025-11-29 16:27:10 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,10 +1,12 @@
 def create_codon_dict(file_path):
+    path= "/content/data/codons.txt"
+    f = open(path,"r")
+    rows = f.readlines()
+    f.close()
     codon_dict = {}
     for lines in rows:
         parts = lines.strip().split('\t')
         codon = parts[0]
         amino = parts[2]
         codon_dict [codon] = amino
-    return (codon_dict)
-
-
+    return codon_dict
